2d239f71c4eb0eb2d27fe2f8f6a8dfda59f90f67
wstrach <wstrach@gmail.com>
2020-05-23 00:23:26 +0200

    helloł2helloł

--- hello.py
+++ hello.py
@@ -1,9 +1,18 @@
+msg="dupa";
+
 print("hello world")
 print("coś tam")
 print("podaj imię : ")
 print("heloł heloł ")
 print("jeszcze jedno heloł")
 print("kolejne heloł 2020-05-07")
-#imie=input();
+#imie=input()
+#nazwisko=input()
+
+print(msg)
+
+msg.capitalize
+
+print(msg)
 
 print("=" * 30)
