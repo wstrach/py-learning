10ef887a7ddb203e1aad95072e9d3eb0010c6877
wstrach <wstrach@gmail.com>
2023-11-07 23:41:34 +0100

    .

--- json_tests001.py
+++ json_tests001.py
@@ -1,1 +1,8 @@
 import json
+
+with open('fcc.json', 'r') as fcc_file:
+    fcc_data = json.load(fcc_file)
+    #print(fcc_data)
+    
+    print(json.dumps(fcc_data, indent=4, sort_keys=True))
+
---
+++ wybierzliczby75.py
@@ -0,0 +1,10 @@
+# z przedziału 100-470 wybierz liczby podzielne przez 7, ale niepodzielne przez 5
+
+numbers57 = [
+liczba 
+for liczba in range(100,471)
+    if (liczba % 7 == 0) and (liczba % 5 != 0)
+]        
+        
+print (numbers57)
+    
