e9b73525d66a50dd6ee6aae91a0e30b2b63bf106
wstrach <wstrach@gmail.com>
2023-11-11 23:58:02 +0100

    .

---
+++ wybierzliczby83.py
@@ -0,0 +1,12 @@
+# z przedziału 100-470 wybierz liczby podzielne przez 7, ale niepodzielne przez 5
+
+# lista generująca []
+numbers83 = [
+liczba 
+for liczba in range(100,471)
+    if (liczba % 8 == 0) and (liczba % 3 != 0)
+]        
+        
+print (numbers83)
+
+
